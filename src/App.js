c97383597fa496d96bf9814ef7d4dc380837a2db
shpaksasha <shpaksasha0000@gmail.com>
2020-02-28 14:45:08 +0200

    changes page home, faq and article,refactor code

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Route} from "react-router-dom";
 import Home from './components/Home/home';
 import Questions from './components/FAQ/faq';
 import One from './components/Article/article_1';
+import Two from './components/Article/article_2';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Route exact path='/' component={Home}/>
       <Route exact path='/faq' component={Questions}/>
       <Route exact path='/faq/article_1' component={One}/>
+      <Route exact path='/faq/article_2' component={Two}/>
     </Router>
   );
 };
--- src/components/FAQ/faq.js
+++ src/components/FAQ/faq.js
@@ -3,40 +3,45 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import {makeStyles} from "@material-ui/core/styles";
-import {Link} from "@material-ui/core";
+import {Container, Link} from "@material-ui/core";
 
-const useStyles = makeStyles(theme =>({
+const useStyles = makeStyles(theme => ({
     root: {
         margin: '50px 10px',
         width: '100%',
-        maxWidth: '700px',
+        minWidth: 'auto',
+        maxWidth: '800px',
         backgroundColor: theme.palette.background.paper,
-    },
-    links:{
-
     }
+
 }));
 
 
-const Questions = () =>{
-   const classes = useStyles();
-    return(
-        <List component="div" className={classes.root} aria-label="mailbox folders">
-            <Link className={classes.links} href='/faq/article_1' color='black' underline='none' >
-            <ListItem  divider>
-                <ListItemText primary="Лион помешал Роналду продлить голевую серию с начала 2020 года" />
-            </ListItem>
-            </Link>
-            <ListItem divider>
-                <ListItemText primary="УЕФА может остановить еврокубки из-за коронавируса. Известны подробности" />
-            </ListItem>
-            <ListItem button divider>
-                <ListItemText primary="Зидан - после поражения от Ман Сити: «Нам не хватило 10 минут»" />
-            </ListItem>
-            <ListItem button divider>
-                <ListItemText primary="Бенфика - Шахтер: онлайн трансляция матча 1/16 Лиги Европы" />
-            </ListItem>
-        </List>
+const Questions = () => {
+    const classes = useStyles();
+    return (
+        <Container maxWidth='md' component='div' fixed>
+            <List component="div" className={classes.root} aria-label="mailbox folders">
+
+                <Link href='/faq/article_1' style={{textDecoration: 'none', color: 'inherit'}}>
+                    <ListItem button divider>
+                        <ListItemText primary="Лион помешал Роналду продлить голевую серию с начала 2020 года"/>
+                    </ListItem></Link>
+
+                <Link href='/faq/article_2' style={{textDecoration: 'none', color: 'inherit'}}>
+                    <ListItem button divider>
+                        <ListItemText
+                            primary="УЕФА может остановить еврокубки из-за коронавируса. Известны подробности"/>
+                    </ListItem></Link>
+
+                <ListItem button divider>
+                    <ListItemText primary="Зидан - после поражения от Ман Сити: «Нам не хватило 10 минут»"/>
+                </ListItem>
+                <ListItem button divider>
+                    <ListItemText primary="Бенфика - Шахтер: онлайн трансляция матча 1/16 Лиги Европы"/>
+                </ListItem>
+            </List>
+        </Container>
     )
 };
 
